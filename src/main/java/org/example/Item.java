8bd9d77612c043f54246900414a946e4a48baa6f
Conny <pedalconny@hotmail.com>
2024-09-04 19:53:49 +0200

    Add Item class with subclasses Treasure and Upgrade

--- src/main/java/org/example/Item.java
+++ src/main/java/org/example/Item.java
@@ -1,4 +1,29 @@
 package org.example;
 
 public class Item {
+    protected int x;
+    protected int y;
+
+    public Item(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Treasure extends Item {
+    private int value;
+
+    public Treasure(int x, int y, int value) {
+        super(x, y);
+        this.value = value;
+    }
+}
+
+class Upgrade extends Item {
+    private String type;
+
+    public Upgrade(int x, int y, String type) {
+        super(x, y);
+        this.type = type;
+    }
 }
