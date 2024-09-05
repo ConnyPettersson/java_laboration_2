33eb530bfe8268d3c5429bffc1f2ce5dee46bd6f
Conny <pedalconny@hotmail.com>
2024-09-05 07:06:37 +0200

    Implement Maze class with grid structure and display method.

--- src/main/java/org/example/Maze.java
+++ src/main/java/org/example/Maze.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class Maze {
+    private char[][] grid;
+
+    public Maze(char[][] grid) {
+        this.grid = grid;
+    }
+
+    public void displayMaze() {
+        for(char[] row :  grid) {
+            for(char cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
 }
